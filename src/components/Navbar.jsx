091b6742ba2476c709b710fa94c964c726ed605d
Benjamin <tangbenjamin123l@gmail.com>
2024-07-25 21:46:27 -0700

    added website content

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import logo from "../assets/bNTG-logo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-9 items-center">
+        <img className="mx-2 w-10" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/benjamin-tang-z35/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/bNTGeez"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const HERO_CONTENT = `I am a developer with a passion to create web applications. My goal is to create helpful applications and deliver exceptional user experiences.`;
+
+//I am an upcoming software engineer honing my skills in HTML/CSS, Javascript, and ReactJS
+
+export const ABOUT_TEXT = `I am a dedicated developer with a passion for creating efficient and user-friendly web applications. I have experience in HTML/CSS, Javascript and ReactJS. My journey in web development began with wondering how I was able to click a button and everything just magically appeared. Outside of coding, I enjoy going to the gym and watching kdramas.`;
+
+export const EXPERIENCES = [{}];
+
+export const PROJECTS = [
+  {
+    title: "Portfolio Website",
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["ReactJS", "TailwindCSS", "Framer Motion"],
+  },
+  {
+    title: "Food Recipe",
+    description:
+      "A platform to searching up different food recipes using the spoonacular API",
+    technologies: ["ReactJS", "Postman"],
+  },
+];
+
+export const CONTACT = {
+  email: "tangbenjamin123@gmail.com",
+};
